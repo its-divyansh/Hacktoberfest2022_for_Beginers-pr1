89143f7c2aa5163f500a994b182863ece5010a78
its-divyansh <91718094+its-divyansh@users.noreply.github.com>
2022-10-29 18:09:40 +0530

    Add files via upload

---
+++ C++/isPowerOfFour.cpp
@@ -0,0 +1,21 @@
+
+// Author: DIVYANSH RAI
+// Aim : To help others and make a pr in hactoberfest
+// Date Modified: 29/10/2022
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPowerOfFour(int n) {
+    // It should be a power of 2 and n-1 should be divisible by 3;
+    return (n && !(n & (n - 1)))&&((n-1)%3==0);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << ((isPowerOfFour(n)) ? "It's a power of 4." : "It's not a power of 4.")<<endl; 
+    return 0;
+}
